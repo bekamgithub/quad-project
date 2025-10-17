37a05b904bd81e40b81795a8d2d53f462fd4fcc1
bekam <benkamau2040@gmail.com>
2025-10-17 16:31:00 +0300

    version 1 quadratic equastion

---
+++ test.py
@@ -0,0 +1,19 @@
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+add = b
+product = a * c
+number_list = [-11, -10, -9, -8, -7, -6, -5, -
+               4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+number_list_two = [-11, -10, -9, -8, -7, -6, -5, -
+                   4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for i in number_list:
+
+    for j in number_list_two:
+        if i * j != product or i + j != add:
+            continue
+        print(i, j)
